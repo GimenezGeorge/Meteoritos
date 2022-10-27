4fa905c047478a9a7edbe249049d5a45b4110bf8
Jorge Gimenez <jorge.2015.8893@gmail.com>
2022-10-27 00:50:29 -0300

    Mejoras_disparo
    Mejoré el disparo. Pero falta el SFX.

--- Juego/Niveles/Naves/Player.gd
+++ Juego/Niveles/Naves/Player.gd
@@ -9,6 +9,8 @@ export var potencia_rotacion:int = 280
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
 
+onready var canion:Canion = $Canion
+
 ## Metodos
 func _integrate_forces(_state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
@@ -32,4 +34,11 @@ func player_input() -> void:
 		dir_rotacion -= 1
 	elif Input.is_action_pressed("rotar_hor"):
 		dir_rotacion += 1
+	
+	# Disparo
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+	
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)
 
---
+++ Juego/Niveles/Nivel.gd
@@ -0,0 +1,19 @@
+class_name Nivel
+extends Node2D
+
+onready var contenedor_proyectiles:Node
+
+func _ready() -> void:
+	conectar_seniales()
+	crear_contenedores()
+
+func conectar_seniales() -> void:
+	Eventos.connect("disparo", self, "_on_disparo")
+
+func crear_contenedores() -> void:
+	contenedor_proyectiles = Node.new()
+	contenedor_proyectiles.name = "ContenedorProyectiles"
+	add_child(contenedor_proyectiles)
+
+func _on_disparo(proyectil:Proyectil) -> void:
+	contenedor_proyectiles.add_child(proyectil)
