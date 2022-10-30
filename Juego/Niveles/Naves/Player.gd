8b31c1877b19c8beefd6a47b9d7508c49e0e8fed
Jorge Gimenez <jorge.2015.8893@gmail.com>
2022-10-30 15:54:23 -0300

    Controlador_estados
    Agregamos controlador de vivición.

--- Juego/Niveles/Naves/Player.gd
+++ Juego/Niveles/Naves/Player.gd
@@ -1,6 +1,9 @@
 class_name Player
 extends RigidBody2D
 
+## Enums
+enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
+
 ## Atributos Export
 export var potencia_motor:int = 20
 export var potencia_rotacion:int = 280
@@ -9,14 +12,24 @@ export var estela_maxima:int = 150
 ## Atributos
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
+var estado_actual:int = ESTADO.SPAWN
 
 onready var canion:Canion = $Canion
 onready var laser:RayoLaser = $LaserBeam2D
 onready var estela:Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx:Motor = $MotorSFX
+onready var colisionador:CollisionShape2D = $CollisionShape2D
 
 ## Metodos
+func _ready() -> void:
+	controlador_estados(estado_actual)
+	##TODO: Quitar, solo DEBUG
+	controlador_estados(ESTADO.VIVO)
+
 func _unhandled_input(event: InputEvent) -> void:
+	if not esta_input_activo():
+		return
+	
 	if event.is_action_pressed("disparo_laser"):
 		laser.set_is_casting(true)
 		
@@ -42,7 +55,34 @@ func _process(_delta: float) -> void:
 	player_input()
 
 ## Metodos Custom
+func controlador_estados(nuevo_estado:int) -> void:
+	match nuevo_estado:
+		ESTADO.SPAWN:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(false)
+		ESTADO.VIVO:
+			colisionador.set_deferred("disabled", false)
+			canion.set_puede_disparar(true)
+		ESTADO.INVENCIBLE:
+			colisionador.set_deferred("disabled", true)
+		ESTADO.MUERTO:
+			colisionador.set_deferred("disabled", true)
+			canion.set_puede_disparar(true)
+			queue_free()
+		_:
+			printerr("Error de estado")
+	
+	estado_actual = nuevo_estado
+
+func esta_input_activo() -> bool:
+	if estado_actual in [ESTADO.MUERTO, ESTADO.SPAWN]:
+		return false
+	
+	return true
+
 func player_input() -> void:
+	if not esta_input_activo():
+		return
 	# Empuje
 	empuje = Vector2.ZERO
 	if Input.is_action_pressed("m_adelante"):
