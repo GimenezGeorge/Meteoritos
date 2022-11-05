51e65467ae4cb374ff26e53a31693dc94bd77acd
Jorge Gimenez <jorge.2015.8893@gmail.com>
2022-11-04 23:02:52 -0300

    Destruccion_player
    Arreglamos el laser, la destruccion del player y otras cositas.

--- Juego/Niveles/Proyectiles/Proyectil.gd
+++ Juego/Niveles/Proyectiles/Proyectil.gd
@@ -11,6 +11,7 @@ func crear(pos: Vector2, dir: float, vel: float, danio_p: int) -> void:
 	position = pos
 	rotation = dir
 	velocidad = Vector2(vel, 0).rotated(dir)
+	danio = danio_p
 
 func _physics_process(delta: float) -> void:
 	position += velocidad * delta
@@ -25,9 +26,7 @@ func _on_VisibilityNotifier2D_screen_exited() -> void:
 	queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.has_method("recibir_danio"):
-		area.recibir_danio(danio)
-		daniar(area)
+	daniar(area)
 
 func _on_body_entered(body: Node) -> void:
 	daniar(body)
