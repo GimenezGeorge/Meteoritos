605cba7edc23c774bbf80d5a1b94c07d2f3e88ab
Jorge Gimenez <jorge.2015.8893@gmail.com>
2022-11-04 23:07:12 -0300

    Eventos_salteado
    Salteamos algunas advertencias.

--- Juego/Autoload/Eventos.gd
+++ Juego/Autoload/Eventos.gd
@@ -2,6 +2,5 @@ extends Node
 
 # warning-ignore:unused_signal
 signal disparo(proyectil)
+# warning-ignore:unused_signal
 signal nave_destruida(posicion, explosiones)
-
-#emit_signal("nave_destruida")
