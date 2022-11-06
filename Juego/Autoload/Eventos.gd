ea811e11f19ec67dc91a96c2e6899992f1bee893
Jorge Gimenez <jorge.2015.8893@gmail.com>
2022-11-06 01:58:59 -0300

    Spawn_aleatorio
    Agregamos Spawn aleatorio para los meteoritos. Falta agregar la destruccion.

--- Juego/Autoload/Eventos.gd
+++ Juego/Autoload/Eventos.gd
@@ -5,5 +5,5 @@ signal disparo(proyectil)
 # warning-ignore:unused_signal
 signal nave_destruida(posicion, explosiones)
 # warning-ignore:unused_signal
-signal spawn_meteorito(posicion, direccion)
+signal spawn_meteorito(posicion, direccion, tamanio)
 
--- Juego/Meteoritos/Meteorito.gd
+++ Juego/Meteoritos/Meteorito.gd
@@ -3,11 +3,36 @@ extends RigidBody2D
 
 export var vel_lineal_base:Vector2 = Vector2(300.0, 300.0)
 export var vel_ang_base:float = 3.0
+export var hitpoints_base:float = 10.0
+
+onready var impacto_sfx = $Impacto_SFX
+
+var hitpoints:float
 
 func _ready() -> void:
 	linear_velocity = vel_lineal_base
 	angular_velocity = vel_ang_base
 
-func crear(pos: Vector2, dir: Vector2) -> void:
+func crear(pos: Vector2, dir: Vector2, tamanio:float) -> void:
 	position = pos
-	linear_velocity = vel_lineal_base * dir
+	mass *= tamanio
+	$Sprite.scale = Vector2.ONE * tamanio
+	#radio = diametro / 2
+	var radio:int = int($Sprite.texture.get_size().x / 2.3 * tamanio)
+	var forma_colision:CircleShape2D = CircleShape2D.new()
+	forma_colision.radius = radio
+	$CollisionShape2D.shape = forma_colision
+	linear_velocity = vel_lineal_base * dir / tamanio
+	angular_velocity = vel_ang_base / tamanio
+	hitpoints = hitpoints_base * tamanio
+
+func recibir_danio(danio: float) -> void:
+	hitpoints -= danio
+	if hitpoints <= 0:
+		destruir()
+		
+	impacto_sfx.play()
+
+func destruir() -> void:
+	$CollisionShape2D.set_deferred("disabled", true)
+	queue_free()
