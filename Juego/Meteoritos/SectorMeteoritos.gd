f7aa8171fa9eacb5b4efaed0ea61770c5175e580
Jorge Gimenez <jorge.2015.8893@gmail.com>
2022-11-08 00:45:25 -0300

    Lluvia_no_llueve
    Está armado el sector de meteoritos, pero no corre como se espera.

--- Juego/Meteoritos/SectorMeteoritos.gd
+++ Juego/Meteoritos/SectorMeteoritos.gd
@@ -1,6 +1,25 @@
 class_name SectorMeteoritos
 extends Node2D
 
-func _on_SpawnTimer_timeout() -> void:
+export var cantidad_meteoritos:int = 10
+
+var spawners:Array
+
+func _ready() -> void:
+	almacenar_spawners()
+
+func almacenar_spawners() -> void:
 	for spawner in $Spawners.get_children():
-		spawner.spawnear_meteorito()
+		spawners.append(spawner)
+
+func spawner_aleatorio() -> int:
+	randomize()
+	return randi() % spawners.size()
+
+func _on_SpawnTimer_timeout() -> void:
+	if cantidad_meteoritos == 0:
+		$SpawnTimer.stop()
+		return
+	
+	spawners[spawner_aleatorio()].spawnear_meteorito()
+	cantidad_meteoritos -= 1
