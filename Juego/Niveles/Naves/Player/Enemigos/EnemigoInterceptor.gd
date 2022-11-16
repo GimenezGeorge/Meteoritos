ff9e06d5a211620909647eb75c8eaac1d7ab32f4
Jorge Gimenez <jorge.2015.8893@gmail.com>
2022-11-16 10:30:31 -0300

    Creando_IA
    Tuve que cerrar para ir a trabajar.

--- Juego/Niveles/Naves/Player/Enemigos/EnemigoInterceptor.gd
+++ Juego/Niveles/Naves/Player/Enemigos/EnemigoInterceptor.gd
@@ -1,2 +1,12 @@
+#EnemigoInterceptor.gd
+class_name EnemigoInterceptor
 extends EnemigoBase
+
+## Enums
+enum ESTADO_IA {IDLE, ATACANDOQ, ATACANDOP, PERSECUCION}
+
+## Atributos
+var estado_ia_actual:int = ESTADO_IA.IDLE
+
+...
 
