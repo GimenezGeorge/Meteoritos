fd834d13149e80ab87b544e9bec9288e4d232669
Jorge Gimenez <jorge.2015.8893@gmail.com>
2022-11-17 01:03:52 -0300

    Enem_orbital_dispara
    Ahora el enemigo orbital dispara. La explosión de la base enemiga, aún no resuelta.

--- Juego/Niveles/Naves/Player/Enemigos/EnemigoOrbital.gd
+++ Juego/Niveles/Naves/Player/Enemigos/EnemigoOrbital.gd
@@ -1,3 +1,15 @@
 class_name EnemigoOrbital
 extends EnemigoBase
 
+export var rango_max_ataque: float = 1400.0
+
+func _ready() -> void:
+	#Temporal
+	canion.set_esta_disparando(true)
+
+func rotar_hacia_player() -> void:
+	.rotar_hacia_player()
+	if dir_player.length() > rango_max_ataque:
+		canion.set_esta_disparando(false)
+	else:
+		canion.set_esta_disparando(true)
