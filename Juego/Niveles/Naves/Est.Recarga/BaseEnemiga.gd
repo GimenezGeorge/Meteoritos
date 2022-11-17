dc1783ee5de2b12639a4f3617581f96bc6987c16
Jorge Gimenez <jorge.2015.8893@gmail.com>
2022-11-17 02:12:14 -0300

    Orbitales_angulares
    Los angulos se ajustaron.

--- Juego/Niveles/Naves/Est.Recarga/BaseEnemiga.gd
+++ Juego/Niveles/Naves/Est.Recarga/BaseEnemiga.gd
@@ -11,6 +11,50 @@ var esta_destruida:bool = false
 func _ready() -> void:
 	$AnimationPlayer.play(elegir_animacion_aleatoria())
 
+# warning-ignore:unused_argument
+func _process(delta: float) -> void:
+	var player_objetivo:Player = DatosJuego.get_player_actual()
+	if not player_objetivo:
+		return
+
+	var dir_player:Vector2 = player_objetivo.global_position - global_position
+	var angulo_player:float = rad2deg(dir_player.angle())
+	print(angulo_player)
+
+func spawnear_orbital() -> void:
+	var pos_spawn:Vector2 = deteccion_cuadrante()
+	
+	var new_orbital:EnemigoOrbital = orbital.instance()
+	new_orbital.crear(
+		global_position + pos_spawn,
+		self
+	)
+	
+	Eventos.emit_signal("spawn_orbital", new_orbital)
+
+func deteccion_cuadrante() -> Vector2:
+	var player_objetivo:Player = DatosJuego.get_player_actual()
+	
+	if not player_objetivo:
+		return Vector2.ZERO
+	
+	var dir_player:Vector2 = player_objetivo.global_position - global_position
+	var angulo_player:float = rad2deg(dir_player.angle())
+	
+	if abs(angulo_player) <= 45.0:
+		#Player entra por la derecha
+		return $PosicionesSpawn/Este.position
+	elif abs(angulo_player) > 135.0 and abs(angulo_player) <= 135.0:
+		#Player entra por a izquierda
+		return $PosicionesSpawn/Oeste.position
+	elif abs(angulo_player) > 45.0 and abs(angulo_player) <= 135.0:
+		#Player entra por arriba o por abajo
+		if sign(angulo_player) > 0:
+			#Player entra por arriba o por abajo
+			return $PosicionesSpawn/Sur.position
+			
+	return $PosicionesSpawn/Norte.position
+
 func elegir_animacion_aleatoria() -> String:
 	randomize()
 	var num_anim:int = $AnimationPlayer.get_animation_list().size() - 1
@@ -46,7 +90,8 @@ func _on_AreaColision_body_entered(body: Node) -> void:
 func _on_VisibilityNotifier2D_screen_entered() -> void:
 	#Spawn Orbital
 	$VisibilityNotifier2D.queue_free()
-	
+	spawnear_orbital()
+
 	var new_orbital:EnemigoOrbital = orbital.instance()
 	new_orbital.crear(
 		global_position + $PosicionesSpawn/Norte.global_position,
